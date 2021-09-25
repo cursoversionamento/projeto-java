9665b7b38c927ab777f1fce73086c14baf4382b6
cursoversionamento <cursoversionamento@gmail.com>
2021-09-25 16:16:02 -0300

    Criação da calculo de valor pago a professor

--- MeuProjetoJava/src/Principal.java
+++ MeuProjetoJava/src/Principal.java
@@ -3,7 +3,13 @@ public class Principal {
 	public static void main(String[] args) {
 		Professor professor = new Professor();
 		professor.setNome("Rafael");
+		professor.setNivelPosgraduacao(NivelPosgraduacao.DOUTOR);
+		double salarioProfessor = professor.calcularValor(10, 100);
+		//10 * 100 = 1000
+		//1000 * 0.3 = 300
+		//1000 + 300 = 1300
+		
 		System.out.println("Hello: "+professor.getNome()+"!");
-		System.out.println("Teste de push");
+		System.out.println("Salario: "+salarioProfessor);
 	}
 }
--- MeuProjetoJava/src/Professor.java
+++ MeuProjetoJava/src/Professor.java
@@ -2,6 +2,7 @@
 public class Professor {
 	private String nome;
 	private String CPF;
+	private NivelPosgraduacao nivelPosgraduacao;
 
 	public String getNome() {
 		return nome;
@@ -19,5 +20,32 @@ public class Professor {
 		CPF = cPF;
 	}
 	
+	
+	
+	public NivelPosgraduacao getNivelPosgraduacao() {
+		return nivelPosgraduacao;
+	}
+
+	public void setNivelPosgraduacao(NivelPosgraduacao nivelPosgraduacao) {
+		this.nivelPosgraduacao = nivelPosgraduacao;
+	}
+
+	public double calcularValor(int qtdHoras, double valorHora)
+	{
+		double bounus = 0;
+		if(this.nivelPosgraduacao == NivelPosgraduacao.ESPECIALIZA)
+		{
+			bounus = 0.1;//10%
+		}else if(this.nivelPosgraduacao == NivelPosgraduacao.MESTRE)
+		{
+			bounus = 0.2;//20%
+		}else if(this.nivelPosgraduacao == NivelPosgraduacao.DOUTOR)
+		{
+			bounus = 0.3;//30%
+		}
+		double salario = qtdHoras * valorHora;
+		double valorMaisBonus = salario + (salario * bounus);
+		return valorMaisBonus;
+	}
 	
 }
